389cec58efd377467ecc783b4d73417841a6e976
admin <denis@1>
2024-10-29 21:57:55 +0700

    Revert "test commit"
    This reverts commit 527d38bb64d1d1ca97c4bc3e59fa7f0ff544c74d.

--- core/clients/api_client.py
+++ core/clients/api_client.py
@@ -75,4 +75,3 @@ class ApiClient:
         with allure.step('Checking status code'):
             assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
         return response.json()
-    # test

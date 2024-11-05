d0d954fc2d20f34956a25e235601f73a3efd4f91
admin <denis@1>
2024-11-05 22:55:15 +0700

    trying create test_create_booking_2

--- tests/test_create_booking_check.py
+++ tests/test_create_booking_check.py
@@ -4,6 +4,6 @@ import requests
 
 @allure.feature('Test create booking')
 @allure.story('Test create booking')
-def test_create_booking(api_client):
+def test_create_booking(api_client, generate_random_booking_data):
     status_code = api_client.create_booking(generate_random_booking_data)
     assert status_code == 200, f"Expected status 200 but got {status_code}"

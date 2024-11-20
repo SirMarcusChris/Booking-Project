7d9e2f60084c98dc064a1f564aa13a75a4a45297
admin <denis@1>
2024-11-20 23:53:54 +0700

    test_create_positive_and_negative_cases

--- tests/test_create_booking_positive_negative.py
+++ tests/test_create_booking_positive_negative.py
@@ -1,3 +1,6 @@
+from nis import match
+from venv import create
+
 import allure
 from pydantic import ValidationError, BaseModel
 from conftest import generate_random_booking_data
@@ -39,6 +42,43 @@ def test_create_booking_with_custom_data(api_client):
         assert response['booking']['bookingdates']['checkin'] == booking_data['bookingdates']['checkin']
     with allure.step('Check create booking bookingdates_checkout'):
         assert response['booking']['bookingdates']['checkout'] == booking_data['bookingdates']['checkout']
+    with allure.step('Check create booking additionalneeds_checkout'):
+        assert response['booking']['additionalneeds'] == booking_data['additionalneeds']
+
+
+@allure.feature('Test creating booking')
+@allure.story('Positive: creating booking with required fields')
+def test_create_booking_with_required_fields(api_client):
+    booking_data = {
+    "firstname": "Anna",
+    "lastname": "Smith",
+    "totalprice": 200,
+    "depositpaid": False,
+    "bookingdates": {
+        "checkin": "2023-12-01",
+        "checkout": "2023-12-10"
+    }
+}
+
+    response = api_client.create_booking(booking_data)
+    try:
+        BookingResponse(**response)
+    except ValidationError as e:
+        raise ValidationError(f"Response validation failed: {e}")
+
+    with allure.step('Check create booking firstname'):
+        assert response['booking']['firstname'] == booking_data['firstname']
+    with allure.step('Check create booking lastname'):
+        assert response['booking']['lastname'] == booking_data['lastname']
+    with allure.step('Check create booking totalprice'):
+        assert response['booking']['totalprice'] == booking_data['totalprice']
+    with allure.step('Check create booking depositpaid'):
+        assert response['booking']['depositpaid'] == booking_data['depositpaid']
+    with allure.step('Check create booking bookingdates_checkin'):
+        assert response['booking']['bookingdates']['checkin'] == booking_data['bookingdates']['checkin']
+    with allure.step('Check create booking bookingdates_checkout'):
+        assert response['booking']['bookingdates']['checkout'] == booking_data['bookingdates']['checkout']
+
 
 @allure.feature('Test creating booking')
 @allure.story('Positive: creating booking with random data')
@@ -61,6 +101,95 @@ def test_create_booking_with_random_data(api_client, generate_random_booking_dat
         assert response['booking']['bookingdates']['checkin'] == generate_random_booking_data['bookingdates']['checkin']
     with allure.step('Check create booking bookingdates_checkout'):
         assert response['booking']['bookingdates']['checkout'] == generate_random_booking_data['bookingdates']['checkout']
+
+
+@allure.feature('Test creating booking')
+@allure.story('Negative: creating booking with empty field')
+def test_create_booking_with_empty_body(api_client, mocker):
+    mock_response = mocker.Mock()
+    mock_response.status_code = 400
+    body = {}
+    mocker.patch.object(api_client.session, 'post', side_effect=Exception("No required fields. Expected status 200 but got 400"))
+    with pytest.raises(Exception, match="No required fields. Expected status 200 but got 400"):
+        api_client.create_booking(body)
+
+
+@allure.feature('Test creating booking')
+@allure.story('Negative: missing empty fields')
+def test_create_booking_with_missing_required_fields(api_client, mocker):
+    mock_response = mocker.Mock()
+    mock_response.status_code = 400
+    body = {
+    "firstname": "John",
+    "lastname": "Doe",
+    "bookingdates": {
+        "checkin": "2023-12-01",
+        "checkout": "2023-12-10"
+    }
+}
+    mocker.patch.object(api_client.session, 'post', side_effect=Exception("The 'totalprice' and 'depositpaid' fields are required. Expected status 200 but got 400"))
+    with pytest.raises(Exception,match="The 'totalprice' and 'depositpaid' fields are required. Expected status 200 but got 400"):
+        api_client.create_booking(body)
+
+@allure.feature('Test creating booking')
+@allure.story('Negative: invalid data type')
+def test_create_booking_with_invalid_data_type(api_client, mocker):
+    mock_response = mocker.Mock()
+    mock_response.status_code = 400
+    body = {
+    "firstname": 123,
+    "lastname": True,
+    "totalprice": "one hundred",
+    "depositpaid": "yes",
+    "bookingdates": {
+        "checkin": "invalid-date",
+        "checkout": "2023-12-10"
+    },
+    "additionalneeds": {}
+}
+    mocker.patch.object(api_client.session, 'post', side_effect=Exception("Required fields are not formatted correctly. Expected status 200 but got 400"))
+    with pytest.raises(Exception, match="Required fields are not formatted correctly. Expected status 200 but got 400"):
+        api_client.create_booking(body)
+
+
+@allure.feature('Test creating booking')
+@allure.story('Negative: logic error checkin > checkout')
+def test_create_booking_with_logic_error(api_client, mocker):
+    mock_response = mocker.Mock()
+    mock_response.status_code = 422
+    body = {
+    "firstname": "Test",
+    "lastname": "User",
+    "totalprice": 100,
+    "depositpaid": True,
+    "bookingdates": {
+        "checkin": "2023-12-10",
+        "checkout": "2023-12-01"
+    }
+}
+    mocker.patch.object(api_client.session, 'post', side_effect=Exception("Checkin date must be before checkout date. Expected status 200 but got 422"))
+    with pytest.raises(Exception, match="Checkin date must be before checkout date. Expected status 200 but got 422"):
+        api_client.create_booking(body)
+
+
+@allure.feature('Test creating booking')
+@allure.story('Negative: creating booking with negative price')
+def test_create_booking_with_negative_price(api_client, mocker):
+    mock_response = mocker.Mock()
+    mock_response.status_code = 400
+    body = {
+    "firstname": "John",
+    "lastname": "Doe",
+    "totalprice": -500,
+    "depositpaid": True,
+    "bookingdates": {
+        "checkin": "2023-12-01",
+        "checkout": "2023-12-10"
+    }
+}
+    mocker.patch.object(api_client.session, 'post', side_effect=Exception("Price cannot be negative. Expected status 200 but got 400"))
+    with pytest.raises(Exception, match="Price cannot be negative. Expected status 200 but got 400"):
+        api_client.create_booking(body)
 
 
 @allure.feature('Test create booking')
